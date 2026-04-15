0057572c8157f6d3210422a8622dd9ef18769882
Jocsan <jocsancj1703@gmail.com>
2026-04-15 13:42:03 -0600

    Cambios en las funciones

--- Interfaz.py
+++ Interfaz.py
@@ -9,13 +9,16 @@ canvaprincipal = tk.Canvas(ventprincipal, width= 742, height= 742, bg= "black") 
 
 canvaprincipal.create_text(371, 310, text= 'Stranger', fill= '#D7271E' , font= ('Stranger Things Outlined', 70)) #Crea el texto que aparece en la ventana
 canvaprincipal.create_text(371, 380, text= 'TEC', fill= '#D7271E' , font= ('Stranger Things Outlined', 67))
-canvaprincipal.create_text(65, 722, text= 'Jocsan Calvo', fill= 'white' , font= ('Benguiat Bold', 10))
-canvaprincipal.create_text(672, 722, text= 'Fabricio Guillén', fill= 'white' , font= ('Benguiat Bold', 10))
+canvaprincipal.create_text(65, 22, text= 'Jocsan Calvo', fill= 'white' , font= ('Benguiat Bold', 10))
+canvaprincipal.create_text(672, 22, text= 'Fabricio Guillén', fill= 'white' , font= ('Benguiat Bold', 10))
 text = canvaprincipal.create_text(371, 500, text= '', fill = 'white' , font= ('Benguiat Bold', 12)) #Variable para que el texto pueda parpadear
 
+estado = {'Parpadear': True} #Variable para controlar el estado del parpadeo
 
 #Función para que el texto indicado parpadee cada cierto tiempo
 def parpadear():
+    if not estado["Parpadear"]:
+        return
     mtexto = canvaprincipal.itemcget(text, 'text') #Obtiene el texto que contiene la variable text
     
     if mtexto == '':
@@ -25,6 +28,21 @@ def parpadear():
         canvaprincipal.itemconfig(text, text= '') #Si ya hay texto, borra el mensaje indicado
     
     ventprincipal.after(750, parpadear) #Hace que la función se ejecute cada 750 milisegundos
+
+
+
+def continuar(event = None):
+    estado["Parpadear"] = False
+    menu()
+   
+def menu():
+    canvaprincipal.delete("all") #Borra todo lo que hay en el canva
+    canvaprincipal.create_text(371, 310, text= 'kkkkkkk', fill= '#D7271E' , font= ('Stranger Things Outlined', 70)) #Crea el texto que aparece en la ventana
+
+
+# Detectar tecla y mouse
+ventprincipal.bind("<Key>", continuar)
+ventprincipal.bind("<Button>", continuar)
 
 parpadear()
 
